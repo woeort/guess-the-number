70d2b7a2063dabab985338574b3c87597626c7ec
woeort <pyntox.channel@yandex.ru>
2023-09-23 17:29:19 +0400

    git add

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿Random rand = new Random();
+int i = rand.Next(2);
+int count = 1;
+Console.WriteLine("Отгадайте число от 0 до 30. У тебя всего лишь 5 попыток. Удачи!");
+Console.WriteLine("Введите число:");
+int k = Convert.ToInt32(Console.ReadLine());
+while (count <= 5)
+{
+    if (i == k)
+    {
+        Console.WriteLine("Победа! Это было число " + k + "!");
+        break;
+    }
+    else
+    {
+        count++;
+        if (count == 5)
+        {
+            Console.WriteLine("Увы, вы не отгадали загаданное число. Это было число " + i + "!");
+            break;
+     
+        }
+        Console.WriteLine("Нет, это не число " + k + "! Попытка № " + count + ":");
+        k = Convert.ToInt32(Console.ReadLine());
+
+    }
+
+}
+Console.ReadLine();
